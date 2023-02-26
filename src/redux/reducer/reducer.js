b735d6d079eb18ea3ff20ceb5dcb03eea26ff409
vin <didekqls93@gmail.com>
2023-02-27 08:58:43 +0900

    Cannot read properties of undefined (reading 'count')

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -4,6 +4,17 @@ let initianState = {
 
 function reducer(state = initianState, action) {
   console.log(action);
+  if (action.type === "INCREMENT") {
+    return;
+    //내가 모르는 state가 있을 수 있으니 기본에 받은 객체를 복사해, 새로운 객체를 변환한다.
+  }
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+    default:
+      return { ...state };
+  }
+  //리턴값이 기본이다.
 }
 
 export default reducer;
