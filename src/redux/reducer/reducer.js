2f58516b8ce73f6147dc7e4a03ec986087214371
vin <didekqls93@gmail.com>
2023-02-27 09:08:40 +0900

    bug fix: 주석처리

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,13 +1,13 @@
-let initianState = {
+let initialState = {
   count: 0,
 };
 
-function reducer(state = initianState, action) {
+function reducer(state = initialState, action) {
   console.log(action);
-  if (action.type === "INCREMENT") {
-    return;
-    //내가 모르는 state가 있을 수 있으니 기본에 받은 객체를 복사해, 새로운 객체를 변환한다.
-  }
+  //   if (action.type === "INCREMENT") {
+  //     return;
+  //     //내가 모르는 state가 있을 수 있으니 기본에 받은 객체를 복사해, 새로운 객체를 변환한다.
+  //   }
   switch (action.type) {
     case "INCREMENT":
       return { ...state, count: state.count + 1 };
