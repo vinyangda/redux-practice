1bb1bf640db2db268a65a78770b35df8260d4b98
vin <didekqls93@gmail.com>
2023-02-28 01:00:28 +0900

    payload 이해하기

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ import Box from "./components/Box";
 
 function App() {
   const count = useSelector((state) => state.count);
+  const id = useSelector((state) => state.id);
+  const password = useSelector((state) => state.password);
   const dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({ type: "INCREMENT" });
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
     //dispatch 가 넘겨주는 Action을 Reducer로 보내준다
     //type은 Action의 이름이다
+
+    //payload
+    // 내가 원하는 특정한 값을 보내기 위해 필요하다.
+  };
+
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "vin", password: "123" } });
   };
   return (
     <div>
+      <h1>
+        {id},{password}
+      </h1>
+
       <div>{count}</div>
       <button onClick={increase}>증가</button>
+      <button onClick={login}>login</button>
+      <button>감소 버튼 만들기</button>
       <Box />
     </div>
   );
